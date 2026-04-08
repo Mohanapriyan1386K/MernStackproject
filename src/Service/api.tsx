032bdaed2aa9d439c8987156c68ec3892a5c9af0
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-04-08 20:39:54 +0530

    live url set completed

--- src/Service/api.tsx
+++ src/Service/api.tsx
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL:"https://mernbackendlast.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
